eb6268c256cecaf39df6adbfd9075cb969a85a1e
RilShadow10 <shadow1337jr@gmail.com>
2025-09-20 22:44:49 +0800

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,8 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game.PlaceId == 108533757090220 or game.PlaceId == 123516946198836 then
+    -- garden tower defense
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/05ccf762a62e738a7dd68f2d72f0c0fe.lua"))()
+end
