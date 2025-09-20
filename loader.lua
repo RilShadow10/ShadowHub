6c959c7880a51eb01927335ccfc8622a12cc7e4f
RilShadow10 <shadow1337jr@gmail.com>
2025-09-20 23:53:45 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,19 @@
+local success, key = pcall(readfile, "ShadowHub/key")
+script_key = success and key or ""
+
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
 task.wait(math.random())
 
-if game.PlaceId == 108533757090220 or game.PlaceId == 123516946198836 then
-    -- garden tower defense
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/05ccf762a62e738a7dd68f2d72f0c0fe.lua"))()
-
+if script_key == "" then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a5292945310574efc23b002f4c1d510.lua"))()
+    return
 end
+
+
+if game.PlaceId == 108533757090220 or game.PlaceId == 123516946198836 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/05ccf762a62e738a7dd68f2d72f0c0fe.lua"))()-- garden tower defense
+end
+
