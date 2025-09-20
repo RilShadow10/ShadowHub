06486d609aeef143b20911fdd298f1976c0f025d
RilShadow10 <shadow1337jr@gmail.com>
2025-09-20 23:57:06 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,8 +12,10 @@ if script_key == "" then
     return
 end
 
+_G.script_key = script_key
 
 if game.PlaceId == 108533757090220 or game.PlaceId == 123516946198836 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/05ccf762a62e738a7dd68f2d72f0c0fe.lua"))()-- garden tower defense
 end
+
 
